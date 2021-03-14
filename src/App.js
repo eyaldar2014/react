1e7bf5dd9fc96b6e73e4f2574d1b0416b6f6595c
Eyal Dar <eyaldar2014@gmail.com>
2021-03-14 13:37:18 +0200

    2.2

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
 
+const data = ["hello", "world"];
+const n1 = 5
+const n2 = 6
+const string = "I love React!"
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          AppleSeeds React App
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Click Me
-        </a>
-      </header>
-    </div>
+    // <div className="App">
+
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       AppleSeeds React App
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Click Me
+    //     </a>
+    //   </header>
+    // </div>
+      <div className="App">
+
+        {data.join(' ')}
+        <br/>
+          {(n1 + n2)}
+      <br/>
+          {'the string length is: ' + string.length}
+      </div>
+
   );
 }
 
