c40096d91c70c17bc05c6cc4ba4a90a7ba31ddfd
Eyal Dar <eyaldar2014@gmail.com>
2021-03-22 11:17:43 +0200

    14.2

--- src/components/14.2/Container.js
+++ src/components/14.2/Container.js
@@ -4,18 +4,66 @@ import './style.css'
 class Container extends react.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            msg: ''
+        }
+
+        this.textInput = react.createRef();
+        this.textInput2 = react.createRef();
     }
 
-    // componentDidMount() {
-    // }
+    componentDidMount() {
+        return this.focusTextInput()
+    }
+
+    focusTextInput = () => {
+        return this.textInput.current.focus();
+    }
+
+    btnFunc=()=>{
+        this.textInput.current.select()
+        document.execCommand("copy")
+    }
+
+    btnFunc2=()=>{
+        this.textInput2.current.select()
+        document.execCommand("paste")
+        this.setState({
+            msg: 'chrome isn\'t compatible with paste command'
+        })
+    }
 
     render() {
 
         return <>
+            <h1>Bla Bla Bla Bla Bla </h1>
+            <br/>
+            <input
+                type="text"
+                ref={this.textInput}
+            />
+            <br/>
+            <input
+                type="button"
+                value={'copy'}
+                className={'btn'}
+                onClick={this.btnFunc}
+            />
+            <br/>
 
+            <textarea
+                className={'textArea'}
+                value={this.state.msg}
+                ref={this.textInput2}
+            />
 
-
-
+            <br/>
+            <input
+                type="button"
+                value={'paste'}
+                className={'btn'}
+                onClick={this.btnFunc2}
+            />
         </>
     }
 }
